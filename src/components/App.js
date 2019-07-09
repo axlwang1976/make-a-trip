d8ba373e6b42b252e210dabc843cbd9cf2692754
Axl Wang <axlwang1976@gmail.com>
2019-07-09 16:00:23 +0800

    find trip logic in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import TripList from './TripList';
 import Trip from './Trip';
 
+const initData = [
+  {
+    id: 1,
+    title: '測試標題',
+    trip: [
+      {
+        day: 1,
+        detail: [
+          { time: '上午', description: '早餐' },
+          { time: '下午', description: '午餐' },
+        ],
+      },
+      {
+        day: 2,
+        detail: [
+          { time: '上午', description: '早餐' },
+          { time: '下午', description: '午餐' },
+        ],
+      },
+    ],
+  },
+];
+
 export default function App() {
+  const findTrip = id => initData.find(trip => trip.id === id);
   return (
     <Switch>
-      {/* <Route exact path="/" render={TripList} /> */}
-      <Route exact path="/trip/:id" render={Trip} />
+      <Route exact path="/" render={() => <TripList />} />
+      <Route
+        exact
+        path="/trip/:id"
+        render={routeProps => (
+          <Trip trip={findTrip(routeProps.match.params.id)} />
+        )}
+      />
     </Switch>
   );
 }
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Day from './Day';
 
-export default function Trip() {
+export default function Trip(props) {
+  console.log(props);
   return <Day />;
 }
