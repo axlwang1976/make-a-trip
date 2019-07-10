72d7db345b0d60830913417a0de6d3e5808b9da4
Axl Wang <axlwang1976@gmail.com>
2019-07-10 09:41:58 +0800

    modify initData and styles

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uuid from 'uuid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import styles from '../styles/Activity.module.scss';
@@ -8,13 +9,16 @@ export default function Activity({ detail }) {
   return (
     <>
       {detail.map(act => (
-        <Paper className={styles.Activity}>
+        <Paper className={styles.Activity} key={uuid()}>
           <Typography variant="h5" gutterBottom>
             {act.time}
           </Typography>
           <Typography variant="body1" gutterBottom>
             {act.description}
           </Typography>
+          <div className={styles.ActivityImg}>
+            <img src={act.imgSrc} alt="img" />
+          </div>
         </Paper>
       ))}
     </>
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Day from './Day';
+import styles from '../styles/Trip.module.scss';
 
 export default function Trip(props) {
-  return <Day {...props} />;
+  const { trip } = props;
+  return (
+    <div className={styles.Trip}>
+      <AppBar position="static" color="primary" className={styles.TripHeader}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            {trip.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Day {...props} />
+    </div>
+  );
 }
+
+Trip.propTypes = {
+  trip: PropTypes.object,
+};
