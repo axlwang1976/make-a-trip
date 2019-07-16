f104da2f0cb65d311d77b6d91a5c600750e7399a
Axl Wang <axlwang1976@gmail.com>
2019-07-16 18:40:40 +0800

    add fade trandition

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MiniTrip from './MiniTrip';
 import styles from '../styles/TripList.module.scss';
+import '../styles/fade.css';
 
 export default function TripList({ trips, history, deleteTrip }) {
   const goToTrip = id => history.push(`/trip/${id}`);
@@ -17,16 +19,13 @@ export default function TripList({ trips, history, deleteTrip }) {
             <Link to="/trip/new">Create Trip</Link>
           </Button>
         </nav>
-        <div className={styles.trips}>
+        <TransitionGroup className={styles.trips}>
           {trips.map(trip => (
-            <MiniTrip
-              {...trip}
-              key={trip.id}
-              goToTrip={goToTrip}
-              deleteTrip={deleteTrip}
-            />
+            <CSSTransition key={trip.id} classNames="fade" timeout={500}>
+              <MiniTrip {...trip} goToTrip={goToTrip} deleteTrip={deleteTrip} />
+            </CSSTransition>
           ))}
-        </div>
+        </TransitionGroup>
       </div>
     </div>
   );
